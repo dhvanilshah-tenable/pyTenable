7d07dab34d8cebfad93806b718418e1e2dc7559e
sahils-tenable <ssingh.ctr@tenable.com>
2021-11-17 22:00:38 +0530

    users api initial commit

--- tenable/entry_point_tenable.py
+++ tenable/entry_point_tenable.py
@@ -1,6 +1,8 @@
 # from tenable.io.v3 import TenableIO
 from tenable.io import TenableIO
-
+'''
+This is a test file for development purpose only
+'''
 
 def tenable_connection():
     '''
@@ -11,10 +13,9 @@ def tenable_connection():
     tio_t.filters.agents_filters()
 
     '''
-    # access_key = '8ebe6067c6a4312cbaf849efffe3ed4829144f0d827bbd1038df11737e8cc631'
-    # secret_key = 'cd7def442bc27f6f543be95cf49a6ecaebf849f00dfdbf87afbd5b19007b5016'
-    access_key = "6625150eaec074271d23d3605cb838eaa8d9d40532e4a602271213389b2ea9af"
-    secret_key = "ca96ce21861d6839dc585692d22c2279a6ed7f12faefd40602f81df3aff73f0a"
+
+    access_key = "0fb9443503651ebd8050a87a5a133ba4665002e703caabde280a887ea0116b20"
+    secret_key = "5f7b11c7411591b569d08201a18795492e4d93d4d64512657925a95e0643616f"
     url = "https://qa-develop.cloud.aws.tenablesecurity.com"
     container_uuid = 'cfdabb09-6aef-481d-b28f-aecb1c38f297'
     tio = TenableIO(access_key=access_key, secret_key=secret_key, url=url)
@@ -30,9 +31,32 @@ def get_users():
 
 def get_users_v3():
     tio = tenable_connection()
-    users = tio.v3.vm.users.details(2236708)
+    # users = tio.v3.vm.users.details(user_id="2236708tes")
+    users = tio.v3.vm.users.edit(user_id=2236708, permissions=12)
     # users = tio.v3.users.list()
     for user in users:
         print(user)
+
+def create_users_v3():
+    tio = tenable_connection()
+    # users = tio.v3.vm.users.details("2236708tes")
+    # users = tio.v3.users.list()
+    user = tio.v3.vm.users.create(username="sahil", password="test", permissions=12)
+    print(user)
+
+def get_networks_v3():
+    tio = tenable_connection()
+    # neto = tio.networks.list(("name", "eq", "Default"))
+    # print(neto)
+    neto = tio.v3.vm.users.search_users(("name", "eq", "sahilsingh"), fields=["name", "uuid", "last_login"],
+                                        limit=10)
+    print(neto)
+
+def export_vulns():
+    tio = tenable_connection()
+    vulns = tio.exports.vulns()
+    for vu in vulns:
+        print(vu)
+    print(vulns)
 
 get_users_v3()
