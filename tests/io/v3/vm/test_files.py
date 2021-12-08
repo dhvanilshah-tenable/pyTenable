da3ea64406760c13cc78ed0e773f5986d4ff04e0
Dhvanil Shah <dhshah.ctr@corp.tenablesecurity.com>
2021-12-08 15:05:22 +0530

    Files API endpoint in v3 TenableIO: Updated way to open file

--- tests/io/v3/vm/test_files.py
+++ tests/io/v3/vm/test_files.py
@@ -5,8 +5,8 @@ import os
 
 import responses
 
-FILE_BASE_URL = r"https://cloud.tenable.com/api/v3/file"
-BASE_URL = r"https://cloud.tenable.com"
+FILE_BASE_URL = 'https://cloud.tenable.com/api/v3/file'
+BASE_URL = 'https://cloud.tenable.com'
 
 
 @responses.activate
@@ -16,18 +16,18 @@ def test_upload(api):
     '''
     responses.add(
         responses.POST,
-        f"{FILE_BASE_URL}/upload",
+        f'{FILE_BASE_URL}/upload',
         json={
-            "fileuploaded": "scan_targets_test.txt"
+            'fileuploaded': 'scan_targets_test.txt'
         }
     )
 
     dummy_file_path = os.path.join(
         os.path.dirname(
             os.path.abspath(__file__)
-            ), "scan_targets_test.txt"
+            ), 'scan_targets_test.txt'
         )
-    fobj = open(dummy_file_path)
-    resp = api.v3.vm.files.upload(fobj, True)
+    with open(dummy_file_path, "r") as fobj:
+        resp = api.v3.vm.files.upload(fobj, True)
 
-    assert resp == "scan_targets_test.txt"
+    assert resp == 'scan_targets_test.txt'
