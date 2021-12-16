9eb68f1844defd7abbc951175f536fb951316eb3
Harsh Shah <hashah.ctr@tenable.com>
2021-12-16 11:44:51 +0000

    renaming test files

--- tests/io/v3/vm/permissions/test_permissions_schema.py
+++ tests/io/v3/vm/permissions/test_permissions_schema.py
@@ -6,7 +6,6 @@ from tenable.io.v3.vm.permissions.schema import PermissionSchema
 
 def test_schema():
     schema = PermissionSchema()
-    payload = schema.dump(schema.load(
-        {'acls': [{'type': 'user', 'id': '2236706', 'permissions': 64}]}))
-    assert payload == {
+    acls = {
         'acls': [{'type': 'user', 'id': 2236706, 'permissions': 64}]}
+    assert schema.dump(schema.load(acls)) == acls
