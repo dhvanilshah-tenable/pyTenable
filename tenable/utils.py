acfc9f6e8b51e16468179a8db7a8bf4b436f635a
parvdesai-tenable <padesai.ctr@tenable.com>
2021-12-20 10:32:17 +0000

    replacing quotes

--- tenable/utils.py
+++ tenable/utils.py
@@ -2,38 +2,38 @@ from restfly.utils import dict_clean, dict_merge, url_validator  # noqa: F401
 
 
 def policy_settings(item):
-    """
+    '''
     Recursive function to attempt to pull out the various settings from scan
     policy settings in the editor format.
-    """
+    '''
     resp = dict()
-    if "id" in item and (
-        "default" in item
+    if 'id' in item and (
+        'default' in item
         or (
-            "type" in item
-            and item["type"]
+            'type' in item
+            and item['type']
             in [
-                "file",
-                "checkbox",
-                "entry",
-                "textarea",
-                "medium-fixed-entry",
-                "password",
+                'file',
+                'checkbox',
+                'entry',
+                'textarea',
+                'medium-fixed-entry',
+                'password',
             ]
         )
     ):
         # if we find both an 'id' and a 'default' attribute, or if we find
         # a 'type' attribute matching one of the known attribute types, then
         # we will parse out the data and append it to the response dictionary
-        if "default" not in item:
-            item["default"] = ""
-        resp[item["id"]] = item["default"]
+        if 'default' not in item:
+            item['default'] = ''
+        resp[item['id']] = item['default']
 
     for key in item.keys():
         # here we will attempt to recurse down both a list of sub-
         # documents and an explicitly defined sub-document within the
         # editor data-structure.
-        if key == "modes":
+        if key == 'modes':
             continue
         if (
             isinstance(item[key], list)
