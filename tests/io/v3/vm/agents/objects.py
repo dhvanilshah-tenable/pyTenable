1cd946e66f775fcd38f6bd9fa7339b381c8e1d36
parvdesai-tenable <padesai.ctr@tenable.com>
2022-02-24 17:13:02 +0530

    bool type in test cases

--- tests/io/v3/vm/agents/objects.py
+++ tests/io/v3/vm/agents/objects.py
@@ -3,9 +3,11 @@ NEGATIVE_CRITERIA_SCHEMA = [
     {'wildcard': 123},
     {'filters': 123},
     {'filters': [123]},
+    {'filters': [False]},
     {'filter_type': 123},
     {'filter_type': 'invalid'},
     {'hardcoded_filters': 123},
+    {'hardcoded_filters': False},
     {'hardcoded_filters': [123]},
     {'invalid': 'value'}
 ]
@@ -13,6 +15,7 @@ NEGATIVE_CRITERIA_SCHEMA = [
 NEGATIVE_DIRECTIVE_SCHEMA = [
     {'invalid': 'key'},
     {'type': 123},
+    {'type': True},
     {'type': 'invalid'},
     {'type': 'restart', 'invalid': 'key'},
     {'type': 'restart', 'option': 'invalid'},
@@ -20,12 +23,14 @@ NEGATIVE_DIRECTIVE_SCHEMA = [
 
 NEGATIVE_AGENT_FILTER_SCHEMA = [
     {'_filters': 123},
+    {'_filters': True},
     {'invalid': 'key'}
 ]
 
 NEGATIVE_AGENT_SCHEMA = [
     {'filters': 'invalid'},
     {'filters': ['invalid']},
+    {'filters': [True]},
     {'filters': [{'invalid': 'key'}]},
     {'invalid': 'key'},
     {'wildcard': 123},
@@ -49,4 +54,5 @@ NEGATIVE_AGENT_SCHEMA = [
     {'criteria': {'invalid': 'key'}},
     {'directive': {'invalid': 'key'}},
     {'directive': 'invalid'},
+    {'directive': False},
 ]
